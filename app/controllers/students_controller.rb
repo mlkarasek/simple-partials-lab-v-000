d21e14e1e4ccdfec863b52ee746f9e29711b3972
Monica  Karasek <mlkarasek@gmail.com>
2018-08-26 01:42:22 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,7 +4,12 @@ class StudentsController < ApplicationController
   end
 
   def create
+    @classroom = Classroom.first 
     @student = Student.new(student_params)
+    @student.name = params [:name]
+    @student.birthday = params[:birthday]
+    @student.hometown - params[:hometown]
+    @student.classroom_id = @classroom.id
     if @student.save
       redirect_to @student
     else
